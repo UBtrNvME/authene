4d76bb303fbec17485e87d7e97357d44652adfd4
Aitemir Kuandyk <mooniron.k@gmail.com>
2021-05-05 23:54:36 +0600

    entities
    Signed-off-by: Aitemir Kuandyk <mooniron.k@gmail.com>

---
+++ authene/auth/entities.py
@@ -0,0 +1,9 @@
+"""Entities that are part of the authorisation and authentication logic."""
+from dataclasses import dataclass
+
+
+@dataclass
+class Credentials:
+    """Credentials object that is used for authentication purposes."""
+
+    username: str
