168a49fbd79b29a8992c5252f2168a7da35934c3
Aitemir Kuandyk <mooniron.k@gmail.com>
2021-05-06 00:22:49 +0600

    password and credential objects
    Signed-off-by: Aitemir Kuandyk <mooniron.k@gmail.com>

--- authene/auth/entities.py
+++ authene/auth/entities.py
@@ -1,10 +1,30 @@
 """Entities that are part of the authorisation and authentication logic."""
-from dataclasses import dataclass
+from typing import Optional
+from uuid import UUID, uuid4
 
 
-@dataclass
 class Credentials:
     """Credentials object that is used for authentication purposes."""
 
-    username: str
-    password: str
+    def __init__(
+        self,
+        username: str,
+        password: str,
+        active: bool = True,
+        uuid: Optional[UUID] = None,
+    ):
+        self.__uuid = uuid or uuid4()
+        self.__password = Password(password)
+        self.__active = active
+
+        self.username = username
+
+
+class Password:
+    """Password object, that provides all of the methods for easy management."""
+
+    def __init__(self, value):
+        self.__value = value
+
+    def __eq__(self, value):
+        pass
