10b058e596b6d2fbd209606902436fabb930e8b1
Aitemir Kuandyk <mooniron.k@gmail.com>
2021-05-23 19:24:52 +0600

    fix in names
    Signed-off-by: Aitemir Kuandyk <mooniron.k@gmail.com>

--- authene/auth/gateways/repository.py
+++ authene/auth/gateways/repository.py
@@ -9,14 +9,14 @@ from abc import ABC, abstractmethod
 class Repository(ABC):
     """Abstract repository class, that defines simple interface over persistent data."""
 
-    def __init__(self, adapter):
+    def __init__(self, dbms_adapter):
         """
         Initialise Repository with database adapter provided.
 
         Classes that inherit from Abstract Repository should use interface provided by
         adapter class.
         """
-        self.__adapter = adapter
+        self.__adapter = dbms_adapter
 
     @abstractmethod
     def create(self, data):
